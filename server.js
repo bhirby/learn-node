9ad49226fabfd3d96fcff4e00122af14028a1b20
Ben Hirby <ben@planetpropaganda.com>
2016-05-23 21:58:19 -0500

    more setup

--- server.js
+++ server.js
@@ -3,15 +3,22 @@ var express = require('express');
 
 // create an express app
 var app = express();
+var ig = require('instagram-node').instagram();
 
-//create an express route for the home page
-//http://localhost:8080/
+// tell node where to look for site resources
+app.use(express.static(__dirname + '/public'));
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+//ROUTES
+http://localhost:8080/
 app.get('/', function (req,res) {
-	res.sendfile('index.html');
+    res.render('pages/index');
 });
 
 //start the server on port 8080
 app.listen(8080);
 
 //send a message
-console.log('Server has started!');
+console.log('App started! Look at http://localhost:8080');
